bb35ba22cbe8330d8065a6175537b76ed7b7bf71
Cheng Xue <cheng.xue910@gmail.com>
2024-01-18 14:35:34 -0800

    initial commit

---
+++ intern/build.gradle.kts
@@ -0,0 +1,16 @@
+/*
+ * Copyright (c) 2023, Glassdoor Inc.
+ *
+ * Licensed under the Glassdoor Inc Hiring Assessment License.
+ * You may not use this file except in compliance with the License.
+ * You must obtain explicit permission from Glassdoor Inc before sharing or distributing this file.
+ * Mention Glassdoor Inc as the source if you use this code in any way.
+ */
+
+plugins {
+    id("com.android.application") version "8.1.2" apply false
+    id("org.jetbrains.kotlin.android") version "1.8.10" apply false
+    id("com.google.devtools.ksp") version "1.9.10-1.0.13" apply false
+    id("com.google.dagger.hilt.android") version "2.48.1" apply false
+    id("org.jetbrains.kotlin.plugin.parcelize") version "1.9.20" apply false
+}
